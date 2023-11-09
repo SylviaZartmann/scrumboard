1f820561cd6237501ad36e6da241ef2fe9884c31
sylviazartmann <sylvi_zartmann@rocketmail.com>
2023-11-09 20:18:25 +0100

    permissions

--- projects/views.py
+++ projects/views.py
@@ -9,14 +9,17 @@ class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
 
+@permission_classes([IsAuthenticated])
 class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
 
+@permission_classes([IsAuthenticated])
 class ContractViewSet(viewsets.ModelViewSet):
     queryset = Contract.objects.all()
     serializer_class = ContractSerializer
 
+@permission_classes([IsAuthenticated])
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
